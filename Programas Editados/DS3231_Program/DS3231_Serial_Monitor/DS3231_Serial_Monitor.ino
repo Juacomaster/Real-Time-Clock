6e54e6cb668c82fa85665cd6f665e67e70af1d7c
Juacomaster <joa.dan.cal@gmail.com>
2024-06-28 01:28:56 +0200

    Holas

---
+++ Programas Editados/DS3231_Program/DS3231_Serial_Monitor/DS3231_Serial_Monitor.ino
@@ -0,0 +1,115 @@
+
+#include <Wire.h>
+#include "ds3231.h"
+#include "rtc_ds3231.h"
+
+#define BUFF_MAX 128
+
+uint8_t time[8];
+char recv[BUFF_MAX];
+unsigned int recv_size = 0;
+unsigned long prev, interval = 1000;
+
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
+
+    memset(recv, 0, BUFF_MAX);
+    Serial.println("GET time");
+
+    //Serial.println("Setting time");
+    //parse_cmd("T300608330032016",16);
+  //           TssmmhhWDDMMYYYY
+    
+}
+
+void loop()
+{
+    char in;
+    char buff[BUFF_MAX];
+    unsigned long now = millis();
+    struct ts t;
+
+    // show time once in a while
+    if ((now - prev > interval) && (Serial.available() <= 0)) {
+        DS3231_get(&t);
+
+        // there is a compile time option in the library to include unixtime support
+#ifdef CONFIG_UNIXTIME
+        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %ld", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec, t.unixtime);
+#else
+        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec);
+#endif
+
+        Serial.println(buff);
+        prev = now;
+    }
+
+    if (Serial.available() > 0) {
+        in = Serial.read();
+
+        if ((in == 10 || in == 13) && (recv_size > 0)) {
+            parse_cmd(recv, recv_size);
+            recv_size = 0;
+            recv[0] = 0;
+        } else if (in < 48 || in > 122) {;       // ignore ~[0-9A-Za-z]
+        } else if (recv_size > BUFF_MAX - 2) {   // drop lines that are too long
+            // drop
+            recv_size = 0;
+            recv[0] = 0;
+        } else if (recv_size < BUFF_MAX - 2) {
+            recv[recv_size] = in;
+            recv[recv_size + 1] = 0;
+            recv_size += 1;
+        }
+
+    }
+}
+
+void parse_cmd(char *cmd, int cmdsize)
+{
+    uint8_t i;
+    uint8_t reg_val;
+    char buff[BUFF_MAX];
+    struct ts t;
+
+    //snprintf(buff, BUFF_MAX, "cmd was '%s' %d\n", cmd, cmdsize);
+    //Serial.print(buff);
+
+    // TssmmhhWDDMMYYYY aka set time
+    if (cmd[0] == 84 && cmdsize == 16) {
+        //T355720619112011
+        t.sec = inp2toi(cmd, 1);
+        t.min = inp2toi(cmd, 3);
+        t.hour = inp2toi(cmd, 5);
+        t.wday = cmd[7] - 48;
+        t.mday = inp2toi(cmd, 8);
+        t.mon = inp2toi(cmd, 10);
+        t.year = inp2toi(cmd, 12) * 100 + inp2toi(cmd, 14);
+        DS3231_set(t);
+        Serial.println("OK");
+    } else if (cmd[0] == 49 && cmdsize == 1) {  // "1" get alarm 1
+        DS3231_get_a1(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 50 && cmdsize == 1) {  // "2" get alarm 1
+        DS3231_get_a2(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 51 && cmdsize == 1) {  // "3" get aging register
+        Serial.print("aging reg is ");
+        Serial.println(DS3231_get_aging(), DEC);
+    } else if (cmd[0] == 65 && cmdsize == 9) {  // "A" set alarm 1
+
+        //ASSMMHHDD
+        for (i = 0; i < 4; i++) {
+            time[i] = (cmd[2 * i + 1] - 48) * 10 + cmd[2 * i + 2] - 48; // ss, mm, hh, dd
+        }
+        uint8_t flags[5] = { 0, 0, 0, 0, 0 };
+        DS3231_set_a1(time[0], time[1], time[2], time[3], flags);
+        DS3231_get_a1(&buff[0], 59);
+        Serial.println(buff);
+
+
+    }}
